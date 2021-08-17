91c2ff34eef5222baf39b1e54a3364b4e90588fc
long ho <long_ho@datahouse.com>
2021-08-17 22:41:40 +0700

    Commit - 17.08.2021

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://b9f35d0f6c78.ngrok.io'
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, useWindowDimensions } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const windowHeight = useWindowDimensions().height;
+
   return (
-    <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input label="Email" />
-      <Spacer />
-      <Input label="Password" />
-      <Spacer>
-        <Button title="Sign up" />
-      </Spacer>
+    <View
+      style={[{ minHeight: Math.round(windowHeight) }]}
+    >
+      <View style={styles.container}>
+        <Spacer>
+          <Text h3>Sign Up for Tracker</Text>
+        </Spacer>
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Spacer />
+        <Input
+          secureTextEntry
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
+        <Spacer>
+          <Button
+            title="Sign up"
+            onPress={() => signup({ email, password })}
+          />
+        </Spacer>
+      </View>
     </View>
   );
 };
@@ -29,7 +57,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    marginBottom: 200
+    marginBottom: 180
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15
   }
 });
 
